14692e5f268de26e63201e513bdf1a9032745d06
mwalters <myron.walters01@gmail.com>
2018-03-20 09:04:52 -0400

    Blackjack Error
    Can't commit Blackjack

--- For_Loops/Blackjack/Dealer.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-
-public class Dealer {
-  private Random random = new Random();
-  private int number;
-
-  public int setNumber(int max){number = random.nextInt(max); return number;}
-}
