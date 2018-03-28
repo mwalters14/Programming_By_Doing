333ad25717ba217d57e90ec6a11423c960f2d220
Myron Walters <myron.walters01@gmail.com>
2018-03-28 18:47:29 -0400

    Nested Loops
    Programming By Doing

---
+++ Nested_Loops/Basic_Nested_Loops/BasicNest.java
@@ -0,0 +1,11 @@
+public class BasicNest {
+  public static void main(String[] args) {
+    for (int i = 0; i <= 5 ; i++ ) {
+      for (int e = 0; e <= 5 ; e++ ) {
+        System.out.print("(" + i + "," + e + ")" + " ");
+      }
+      System.out.println("");
+    }
+    // System.out.println(" ");
+  }
+}
