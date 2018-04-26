7accd2bcbfcd3579db36993709bbfd512c65e0e4
Myron Walters <myron.walters01@gmail.com>
2018-04-25 21:31:57 -0400

    ArrayList
    Programming By Doing

--- ArrayLists/src/com/pbd/String_Sort/SortStrings.java
+++ ArrayLists/src/com/pbd/String_Sort/SortStrings.java
@@ -9,10 +9,44 @@ public class SortStrings {
         strings.add("Allie");
         strings.add("Leo");
         strings.add("Apple");
-        strings.add("Apple");
-        strings.add("Apple");
-        strings.add("");
+        strings.add("Orange");
+        strings.add("Pear");
+
+
+        System.out.println("Unsorted: " + strings);
+        bubble_sort(strings);
+        System.out.println("Sorted:   " + strings);
+
+
     }
 
+    private static void bubble_sort(ArrayList<String> array) {
+        boolean swapped = true;
+        int j = 0;
+        while (swapped) {
+            swapped = false;
+            j++;
 
+            for (int i = 0; i < array.size() - j ; i++) {
+                String temp = array.get(i);
+                String swap = array.get(i+1);
+
+                if (temp.compareTo(swap) > 0) {
+                    swap(array,i);
+                    swapped = true;
+                }
+            }
+        }
+    }
+
+    private static void swap(ArrayList<String> array, int i ) {
+        String temp = array.get(i);
+        String swap = array.get(i+1);
+
+        array.remove(i);
+        array.add(i, temp);
+
+        array.remove(i+1);
+        array.add(i, swap);
+    }
 }
