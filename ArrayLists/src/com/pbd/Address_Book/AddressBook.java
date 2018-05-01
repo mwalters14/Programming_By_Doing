0d5f0f8eaac1382f25f8843fdc6f3b1a5a608361
Myron Walters <myron.walters01@gmail.com>
2018-04-30 21:32:48 -0400

    Address Book
    Added Functions
    
    Save
    Edit
    Sort
    Search
    
    TODO: Check for bad inputs

--- ArrayLists/src/com/pbd/Address_Book/AddressBook.java
+++ ArrayLists/src/com/pbd/Address_Book/AddressBook.java
@@ -1,8 +1,6 @@
 package com.pbd.Address_Book;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,12 +12,13 @@ class   Record {
 }
 
 public class AddressBook {
+    static Scanner keyboard = new Scanner(System.in);
     public static void main(String[] args) {
         int size = 5;
         int choice = 0;
 
         ArrayList<Record> records = new ArrayList<Record>(size);
-        Scanner keyboard = new Scanner(System.in);
+
 
 
         while (choice != 9) {
@@ -38,42 +37,31 @@ public class AddressBook {
             choice = keyboard.nextInt();
 
             if (choice == 1) {
-                System.out.print("What is the path to the file you would like to load: ");
-                String path = keyboard.next();
-                load_file(records, path);
-                System.out.println();
+                load_file(records);
+            }
+
+            if (choice == 2) {
+                save_toFile(records);
             }
 
             if (choice == 3) {
-                Record record = new Record();
-                System.out.println("Add Record");
-
-                System.out.print("\tFirst Name: ");
-                record.first_name = keyboard.next();
-
-                System.out.print("\tLast Name: ");
-                record.last_name = keyboard.next();
-
-                System.out.print("\tPhone Number: ");
-                record.phone_number = keyboard.next();
-
-                System.out.print("\tAddress: ");
-                record.address = keyboard.nextLine();
-                keyboard.nextLine();
-
-                System.out.println();
-
-
-                records.add(record);
+                add_record(records);
             }
 
             if (choice == 4) {
-                display_record(records);
-                System.out.print("Which record to remove: ");
-                int record_toRemove = keyboard.nextInt();
-                records.remove(record_toRemove);
-                System.out.println("Record has been removed successfully");
-                display_record(records);
+                remove_record(records);
+            }
+
+            if (choice == 5) {
+                edit_record(records);
+            }
+
+            if (choice == 6) {
+                sort_records(records);
+            }
+
+            if (choice == 7) {
+                search_record(records);
             }
 
             if (choice == 8) {
@@ -81,9 +69,38 @@ public class AddressBook {
             }
         }
         System.out.println("Thanks for using the Address Book program, Goodbye!");
+        keyboard.close();
     }
 
-    public static void load_file(ArrayList<Record> records, String path) {
+
+    public static void add_record(ArrayList<Record> records) {
+        Record record = new Record();
+        System.out.println("Add Record");
+
+        System.out.print("\tFirst Name: ");
+        record.first_name = keyboard.next();
+
+        System.out.print("\tLast Name: ");
+        record.last_name = keyboard.next();
+
+        System.out.print("\tPhone Number: ");
+        record.phone_number = keyboard.next();
+        keyboard.nextLine();
+
+        System.out.print("\tAddress: ");
+        record.address = keyboard.nextLine();
+
+        System.out.println();
+
+
+        records.add(record);
+    }
+
+    public static void load_file(ArrayList<Record> records) {
+        System.out.print("What is the path to the file you would like to load: ");
+        String path = keyboard.next();
+        System.out.println();
+
         Scanner sc = read_file(path);
         while (sc.hasNextLine()) {
             Record record = new Record();
@@ -91,16 +108,211 @@ public class AddressBook {
             record.last_name = sc.next();
             record.phone_number = sc.next();
             record.address = sc.next();
+            records.add(record);
+
             if (sc.nextLine().isEmpty()) {
                 continue;
             }
-            records.add(record);
+
         }
         sc.close();
     }
 
+    public static void remove_record(ArrayList<Record> records) {
+        display_record(records);
+        System.out.print("Which record to remove: ");
+        int record_toRemove = keyboard.nextInt();
+        records.remove(record_toRemove);
+        System.out.println("Record has been removed successfully");
+        display_record(records);
+    }
 
-    public static Scanner read_file(String filename) {
+    public static void edit_record(ArrayList<Record> records) {
+        int options = 0;
+        int entry = 0;
+
+        display_record(records);
+        System.out.println();
+
+        System.out.print("What entry would you like to edit: ");
+        entry = keyboard.nextInt();
+
+        System.out.println("What would you like to edit on entry " + entry);
+
+        System.out.println("Options");
+        System.out.println("1)\tFirst Name");
+        System.out.println("2)\tLast Name");
+        System.out.println("3)\tPhone Number");
+        System.out.println("4)\tAddress");
+
+        System.out.print("Option: ");
+        options = keyboard.nextInt();
+        String change;
+        switch (options) {
+            case 1:
+                System.out.println("What would you like to change the first name to?");
+                System.out.print("Change: ");
+                change = keyboard.next();
+
+                records.get(entry).first_name = change;
+                System.out.println("First name changed successfully");
+                break;
+            case 2:
+                System.out.println("What would you like to change the last name to?");
+                System.out.print("Change: ");
+                change = keyboard.next();
+
+                records.get(entry).last_name = change;
+                System.out.println("Last name changed successfully");
+                break;
+            case 3:
+                System.out.println("What would you like to change the phone number to?");
+                System.out.print("Change: ");
+                change = keyboard.next();
+
+                records.get(entry).phone_number = change;
+                System.out.println("Phone number changed successfully");
+                break;
+            case 4:
+                System.out.println("What would you like to change the address to?");
+                System.out.print("Change: ");
+                change = keyboard.next();
+
+                records.get(entry).address = change;
+                System.out.println("Address changed successfully");
+                break;
+        }
+    }
+
+    public static void sort_records(ArrayList<Record> records) {
+        System.out.println("How would you like to sort the records?");
+        System.out.println("1)\tFirst Name");
+        System.out.println("2)\tLast Name");
+        System.out.println("3)\tPhone Number");
+        System.out.println("4)\tAddress");
+
+        int sortBy = keyboard.nextInt();
+
+        switch (sortBy) {
+            case 1:
+                Sort.sort_firstName(records);
+            case 2:
+                Sort.sort_lastName(records);
+            case 3:
+                Sort.sort_phoneNumber(records);
+            case 4:
+                Sort.sort_Address(records);
+        }
+    }
+
+    private static class Sort {
+        private static void sort_firstName(ArrayList<Record> records) {
+            boolean swapped = true;
+            int j = 0;
+
+            while (swapped) {
+                swapped = false;
+                j++;
+
+                for (int i = 0; i < records.size() - j ; i++) {
+                    String temp = String.valueOf(records.get(i).first_name.charAt(0));
+                    String swap = String.valueOf(records.get(i+1).first_name.charAt(0));
+
+                    if (temp.compareTo(swap) >= 0) {
+                        swap(records,i);
+                        swapped = true;
+                    }
+                }
+            }
+        }
+        private static void sort_lastName(ArrayList<Record> records) {
+            boolean swapped = true;
+            int j = 0;
+
+            while (swapped) {
+                swapped = false;
+                j++;
+
+                for (int i = 0; i < records.size() - j ; i++) {
+                    String temp = String.valueOf(records.get(i).last_name.charAt(0));
+                    String swap = String.valueOf(records.get(i+1).last_name.charAt(0));
+
+                    if (temp.compareTo(swap) >= 0) {
+                        swap(records,i);
+                        swapped = true;
+                    }
+                }
+            }
+        }
+        private static void sort_phoneNumber(ArrayList<Record> records) {
+            boolean swapped = true;
+            int j = 0;
+
+            while (swapped) {
+                swapped = false;
+                j++;
+
+                for (int i = 0; i < records.size() - j ; i++) {
+                    String[] one = records.get(i).phone_number.split("-");
+                    String[] two = records.get(i+1).phone_number.split("-");
+
+                    int temp = Integer.parseInt(one[0]);
+                    int swap = Integer.parseInt(two[0]);
+
+                    System.out.println(temp);
+                    System.out.println(swap);
+
+                    if (temp < swap) {
+                        swap(records,i);
+                        swapped = true;
+                    }
+                }
+            }
+        }
+        private static void sort_Address(ArrayList<Record> records) {
+            boolean swapped = true;
+            int j = 0;
+
+            while (swapped) {
+                swapped = false;
+                j++;
+
+                for (int i = 0; i < records.size() - j ; i++) {
+                    String temp = String.valueOf(records.get(i).address.charAt(0));
+                    String swap = String.valueOf(records.get(i+1).address.charAt(0));
+
+                    if (temp.compareTo(swap) >= 0) {
+                        swap(records,i);
+                        swapped = true;
+                    }
+                }
+            }
+        }
+        private static void swap(ArrayList<Record> records, int i ) {
+            Record temp = records.get(i);
+            Record swap = records.get(i+1);
+
+            records.remove(i);
+            records.add(i, temp);
+
+            records.remove(i+1);
+            records.add(i, swap);
+        }
+    }
+
+    private static void search_record(ArrayList<Record> records) {
+        System.out.print("Which entry would you like to search for? ");
+        int search = keyboard.nextInt();
+
+        System.out.println("Record " + search);
+        System.out.println("\tFirst Name: " + records.get(search).first_name);
+        System.out.println("\tLast Name: " + records.get(search).last_name);
+        System.out.println("\tPhone Number: " + records.get(search).phone_number);
+        System.out.println("\tAddress: " + records.get(search).address);
+        System.out.println();
+    }
+
+    private static Scanner read_file(String filename) {
         File file = new File(filename);
         Scanner sc = null;
         try {
@@ -111,7 +323,39 @@ public class AddressBook {
         return sc;
     }
 
-    public static void test_record(ArrayList<Record> records) {
+    private static void save_toFile(ArrayList<Record> records) {
+        System.out.print("What shall we call the name of this file: ");
+        String filename = keyboard.next();
+
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(new FileWriter(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File f = new File(filename);
+        System.out.println("File location: " + f.getAbsolutePath());
+        System.out.println("Saving file....");
+
+        int size = records.size();
+        for (int i = 0; i < records.size(); i++) {
+            writer.println(records.get(i).first_name);
+            writer.println(records.get(i).last_name);
+            writer.println(records.get(i).phone_number);
+            writer.println(records.get(i).address);
+
+            size--;
+            if (size != 0) {
+                writer.println("");
+            }
+        }
+
+        System.out.println("File saved!\n");
+        writer.close();
+    }
+
+    private static void test_record(ArrayList<Record> records) {
         Record record = new Record();
         record.first_name = "Myron";
         record.last_name = "Walters";
@@ -120,7 +364,7 @@ public class AddressBook {
         records.add(record);
     }
 
-    public static void display_record(ArrayList<Record> records){
+    private static void display_record(ArrayList<Record> records){
         for (int i = 0; i < records.size(); i++) {
             System.out.println("Record " + (i+1));
             System.out.println("\tFirst Name: " + records.get(i).first_name);
